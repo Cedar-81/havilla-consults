1b28664f500b93bdc4150a8f1ff7fb20c60e1df5
Cedar-81 <cedarwudnovels@gmail.com>
2024-02-02 17:03:10 +0100

    minor bug fix

--- src/components/booking.svelte
+++ src/components/booking.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mt-24 mx-5 lg:mx-[80px] space-y-12 mb-20 ">
-    <h1 class="text-2xl lg:text-4xl font-bold text-brand-light text-center lg:text-brand-dark">
+    <h1 class="text-2xl lg:text-4xl font-bold text-center text-brand-dark">
         Pick a time that works for you let’s talk and get you <br class="hidden lg:block" /> on the right path for your future
     </h1>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 items-center">
