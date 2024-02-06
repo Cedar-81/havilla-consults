61e2150912c2b2da4a9a4eb00bf0bd313d1f634f
Cedar-81 <cedarwudnovels@gmail.com>
2024-02-06 23:24:30 +0100

    made api updates

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,8 @@
-<div class="flex justify-between items-center bg-black text-white px-5 py-4 relative top-[10rem] lg:px-[80px] ">
+<div class="flex flex-col items-start space-y-10 lg:space-y-0 lg:flex-row justify-between lg:items-center bg-black text-white px-5 py-4 relative top-[10rem] lg:px-[80px] ">
+    
     <img class="h-[33px]" src="/assets/logo.png" alt="havilla logo" />
 
-    <div class="space-y-4">
+    <div class="space-y-4 max-w-full">
         <ul class="flex space-x-8">
             <a href="/"><li class="text-lg hover:text-brand-light cursor-pointer">Home</li></a>
             <a href="/about"><li class="text-lg hover:text-brand-light cursor-pointer">About</li></a>
